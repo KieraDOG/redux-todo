d1b1a3d3a95e11bf4f90db91c962d956eae414d5
Long Zhao <zlong@outlook.com>
2020-06-25 11:11:57 +1000

    Updates

---
+++ precache-manifest.eec8537c23d5784b621dbfe253f926ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8dcb3e1b71c0f0e59713657a7b1ccb",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "ca3aa50fd9fe4fa9618f",
+    "url": "/redux-todo/static/css/main.2876dd84.chunk.css"
+  },
+  {
+    "revision": "9ef841630339c8b4a03b",
+    "url": "/redux-todo/static/js/2.a42334a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.a42334a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3aa50fd9fe4fa9618f",
+    "url": "/redux-todo/static/js/main.420d38e5.chunk.js"
+  },
+  {
+    "revision": "e0cfc32e20de222d9efb",
+    "url": "/redux-todo/static/js/runtime-main.140b26e7.js"
+  }
+]);
